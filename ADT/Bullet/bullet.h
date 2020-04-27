eaf6e5f4856cb6ab0dcda6ca023706483cf0134c
qriticual <62610168+qriticual@users.noreply.github.com>
2020-04-27 21:30:55 +0200

    Update bullet.h

--- ADT/Bullet/bullet.h
+++ ADT/Bullet/bullet.h
@@ -1,3 +1,4 @@
+
 #pragma once
 
 #ifndef bullet_h
@@ -8,9 +9,14 @@
 
 typedef struct Bullet_type *Bullet;
 
-Bullet createBullet(int x, int y);
-int getBulletPositionX(Bullet b);
-int getBulletPositionY(Bullet b);
-int destroyBullet(Bullet b);
+Bullet createBullet(float x, float y, float dy, float dx, float a);
+float getBulletPositionX(Bullet b);
+float getBulletPositionY(Bullet b);
+void updateBulletPositionX(Bullet b, float x);
+void updateBulletPositionY(Bullet b, float y);
+void destroyBullet(Bullet *bullets[], int i);
+float getBulletDy(Bullet b);
+float getBulletDx(Bullet b);
+float getBulletAngle(Bullet b);
 
 #endif /* Bullet_h */
