d9af404623a0b2b7e66ec379e2b0900d8090a949
qriticual <62610168+qriticual@users.noreply.github.com>
2020-04-27 21:31:32 +0200

    Update tank.c

--- ADT/Tank/tank.c
+++ ADT/Tank/tank.c
@@ -4,38 +4,112 @@
 #define PRIVATE static
 
 struct Tank_type {
-	int TANK_POSITION_X;
-	int TANK_POSITION_Y;
-	int TANK_FRAME;
-	int speed;
+	float TANK_POSITION_X;
+	float TANK_POSITION_Y;
+	float TANK_VELOCITY;
+	float TANK_RADIAN;
+	float TANK_ANGLE;
+	float TANK_DX;
+	float TANK_DY;
+	int TIMER;
 };
 
-PUBLIC Tank createTank(int x, int y) {
+PUBLIC Tank createTank(float x, float y) {
 	Tank t = malloc(sizeof(struct Tank_type));
 	t->TANK_POSITION_X = x;
 	t->TANK_POSITION_Y = y;
-	t->TANK_FRAME = 0;
-	t->speed = 1;
+	t->TANK_VELOCITY = 0;
+	t->TANK_RADIAN = 0;
+	t->TANK_ANGLE = 0;
+	t->TANK_DX = 0;
+	t->TANK_DY = 0;
+	t->TIMER = 0;
+
 	return t;
 }
 
-PUBLIC int getTankPositionX(Tank t) {
+PUBLIC float getTankPositionX(Tank t) {
 	return t->TANK_POSITION_X;
 }
 
-PUBLIC int getTankPositionY(Tank t) {
+PUBLIC float getTankPositionY(Tank t) {
 	return t->TANK_POSITION_Y;
 }
 
-PUBLIC void updateTankPositionX(Tank t, int x) {
+PUBLIC void updateTankPositionX(Tank t, float x) {
 	t->TANK_POSITION_X += x;
 }
 
-PUBLIC void updateTankPositionY(Tank t, int y) {
+PUBLIC void updateTankPositionY(Tank t, float y) {
 	t->TANK_POSITION_Y += y;
 }
 
-PUBLIC int destroyTank(Tank t) {
+PUBLIC float destroyTank(Tank t) {
 	free(t);
 	return 1;
+}
+
+PUBLIC float setTankY(Tank t, float y)
+{
+	t->TANK_POSITION_Y = y;
+}
+
+PUBLIC float setTankX(Tank t, float x)
+{
+	t->TANK_POSITION_X = x;
+}
+
+PUBLIC float getTankAngle(Tank t) {
+	return t->TANK_ANGLE;
+}
+
+PUBLIC void updateTankAngle(Tank t, float x) {
+	t->TANK_ANGLE = t->TANK_ANGLE + x;
+}
+
+PUBLIC void setTankAngle(Tank t, float x)
+{
+	t->TANK_ANGLE = x;
+}
+
+PUBLIC float getTankVelocity(Tank t) {
+	return t->TANK_VELOCITY;
+}
+
+PUBLIC void setTankVelocity(Tank t, float x) {
+	t->TANK_VELOCITY = x;
+}
+
+PUBLIC float getTankRadian(Tank t) {
+	return t->TANK_RADIAN;
+}
+
+PUBLIC void setTankRadian(Tank t, float x) {
+	t->TANK_RADIAN = x;
+}
+
+PUBLIC float getTankDx(Tank t) {
+	return t->TANK_DX;
+}
+
+PUBLIC void setTankDx(Tank t, float x) {
+	t->TANK_DX = x;
+}
+
+PUBLIC float getTankDy(Tank t) {
+	return t->TANK_DY;
+}
+
+PUBLIC void setTankDy(Tank t, float x) {
+	t->TANK_DY = x;
+}
+
+PUBLIC void setTimer(Tank t, int x)
+{
+	t->TIMER = x;
+}
+
+PUBLIC int getTimer(Tank t)
+{
+	return t->TIMER;
 }
