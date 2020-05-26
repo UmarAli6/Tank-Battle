6bf7651a466630f19e01d1b4a306b27fdedba518
Rabisu99 <62613731+Rabisu99@users.noreply.github.com>
2020-05-26 22:28:28 +0200

    Update bullet.c

--- finalRelease/client/bullet.c
+++ finalRelease/client/bullet.c
@@ -4,63 +4,63 @@
 #define PRIVATE static
 
 struct Bullet_type {
-    float BULLET_POSITION_X;
-    float BULLET_POSITION_Y;
-    float BULLET_DY;
-    float BULLET_DX;
-    float BULLET_ANGLE;
-    int OWNER;
+	float BULLET_POSITION_X;
+	float BULLET_POSITION_Y;
+	float BULLET_DY;
+	float BULLET_DX;
+	float BULLET_ANGLE;
+	int OWNER;
 };
 
 PUBLIC Bullet createBullet(float x, float y, float dy, float dx, float a, int o) {
-    Bullet b = malloc(sizeof(struct Bullet_type));
-    b->BULLET_POSITION_X = x;
-    b->BULLET_POSITION_Y = y;
-    b->BULLET_DY = dy * -8;
-    b->BULLET_DX = dx * -8;
-    b->BULLET_ANGLE = a;
-    b->OWNER = o; //Called entity
-    return b;
+	Bullet b = malloc(sizeof(struct Bullet_type));
+	b->BULLET_POSITION_X = x + 20;
+	b->BULLET_POSITION_Y = y + 24;
+	b->BULLET_DY = dy * -8;
+	b->BULLET_DX = dx * -8;
+	b->BULLET_ANGLE = a;
+	b->OWNER = o; //Called entity
+	return b;
 }
 
 PUBLIC void updateBulletPositionX(Bullet b, float x)
 {
-    b->BULLET_POSITION_X += x;
+	b->BULLET_POSITION_X += x;
 }
 
 PUBLIC void updateBulletPositionY(Bullet b, float y)
 {
-    b->BULLET_POSITION_Y += y;
+	b->BULLET_POSITION_Y += y;
 }
 
 PUBLIC float getBulletPositionX(Bullet b) {
-    return b->BULLET_POSITION_X;
+	return b->BULLET_POSITION_X;
 }
 
 PUBLIC float getBulletPositionY(Bullet b) {
-    return b->BULLET_POSITION_Y;
+	return b->BULLET_POSITION_Y;
 }
 
 PUBLIC void destroyBullet(Bullet bullets[], int i) {
-    free(bullets[i]);
-    bullets[i] = NULL;
+	free(bullets[i]);
+	bullets[i] = NULL;
 }
 
 PUBLIC float getBulletDx(Bullet b)
 {
-    return b->BULLET_DX;
+	return b->BULLET_DX;
 }
 
 PUBLIC float getBulletDy(Bullet b)
 {
-    return b->BULLET_DY;
+	return b->BULLET_DY;
 }
 
 PUBLIC float getBulletAngle(Bullet b)
 {
-    return b->BULLET_ANGLE;
+	return b->BULLET_ANGLE;
 }
 
 PUBLIC int bulletOwner(Bullet b) {
-    return b->OWNER;
+	return b->OWNER;
 }
